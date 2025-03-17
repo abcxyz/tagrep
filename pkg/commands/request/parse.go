d0f2793366e9d64454976b54e0537f899e0b7222
Dylan Yurgionas <dcreey@google.com>
2025-03-17 15:29:54 +0000

    fix: lint

--- pkg/commands/request/parse.go
+++ pkg/commands/request/parse.go
@@ -104,7 +104,7 @@ func (c *ParseCommand) Process(ctx context.Context) (merr error) {
 		"tags", ts)
 
 	for _, t := range ts {
-		if err := c.Stdout().Write([]byte(fmt.Sprintf("%s=%s", t.Name, t.Value))); err != nil {
+		if _, err := c.Stdout().Write([]byte(fmt.Sprintf("%s=%s", t.Name, t.Value))); err != nil {
 			merr = errors.Join(merr, fmt.Errorf("failed to write tag(%s): %w", t.Name, err))
 		}
 	}
